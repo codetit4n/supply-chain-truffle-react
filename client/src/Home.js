821360689df2415ee3cf5c284e263db2ccb10baf
Arya-Vats <89343269+arya-vats@users.noreply.github.com>
2023-05-07 01:34:39 +0530

    initial-revamp the styling

--- client/src/Home.js
+++ client/src/Home.js
@@ -16,24 +16,34 @@ function Home() {
         history.push('/track')
     }
     return (
-        <div>
+        <div className='container'>
+            <div className='header'>
             <h3>Pharmaceutical Supply Chain Flow :- </h3>
+            </div>
             <br />
+            <div className='register'>
             <h6>(Note: Here <u>Owner</u> is the person who deployed the smart contract on the blockchain)</h6>
             <h5>Step 1: Owner Should Register Raw material suppliers ,Manufacturers, Distributors and Retailers</h5>
             <h6>(Note: This is a one time step. Skip to step 2 if already done)</h6>
             <button onClick={redirect_to_roles} className="btn btn-outline-primary btn-sm">Register</button>
+            </div>
             <br />
+            <div className='ordermedicines'>
             <h5>Step 2: Owner should order medicines</h5>
             <button onClick={redirect_to_addmed} className="btn btn-outline-primary btn-sm">Order Medicines</button>
+            </div>
             <br />
+            <div className='controlchain'>
             <h5>Step 3: Control Supply Chain</h5>
             <button onClick={redirect_to_supply} className="btn btn-outline-primary btn-sm">Control Supply Chain</button>
+            </div>
             <br />
             <hr />
             <br />
+            <div className='track'>
             <h5><b>Track</b> the medicines:</h5>
             <button onClick={redirect_to_track} className="btn btn-outline-primary btn-sm">Track Medicines</button>
+            </div>
         </div>
     )
 }
