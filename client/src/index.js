28655aebe7229fd74d3390f56c6be9985146b389
codeTIT4N <lokeshkumar636@yahoo.com>
2023-05-14 02:16:32 +0530

    fixed node issue

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
